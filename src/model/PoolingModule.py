86a9db2848be167ec91c3ad0305f3381d0a78558
Roshan Daivajna <roshan.daivajna@gmail.com>
2021-08-15 11:04:03 +0530

    Add Pooling Module

--- src/model/PoolingModule.py
+++ src/model/PoolingModule.py
@@ -1,11 +1,69 @@
 import torch
 from torch import nn
 
-device = 'cuda' if torch.cuda.is_available else 'cpu'
-
 class PoolingModule(nn.Module):
-    def __init__(self):
+    """Pooling module as proposed in the Social GAN paper"""
+    def __init__(
+        self, embedding_dim=64, hidden_dim=64, mlp_dim=1024, bottleneck_dim=1024,
+        activation='relu', batch_norm=True, dropout=0.0
+    ):
         super(PoolingModule, self).__init__()
-    
-    def forward(self):
-        pass
+
+        self.mlp_dim = 1024
+        self.hidden_dim = hidden_dim
+        self.bottleneck_dim = bottleneck_dim
+        self.embedding_dim = embedding_dim
+
+        mlp_pre_dim = embedding_dim + hidden_dim
+        mlp_pre_pool_dims = [mlp_pre_dim, 512, bottleneck_dim]
+
+        self.spatial_embedding = nn.Linear(2, embedding_dim)
+        self.mlp_pre_pool = make_mlp(
+            mlp_pre_pool_dims,
+            activation=activation,
+            batch_norm=batch_norm,
+            dropout=dropout)
+
+    def repeat(self, tensor, num_reps):
+        """
+        Inputs:
+        -tensor: 2D tensor of any shape
+        -num_reps: Number of times to repeat each row
+        Outpus:
+        -repeat_tensor: Repeat each row such that: R1, R1, R2, R2
+        """
+        col_len = tensor.size(1)
+        tensor = tensor.unsqueeze(dim=1).repeat(1, num_reps, 1)
+        tensor = tensor.view(-1, col_len)
+        return tensor
+
+    def forward(self, h_states, seq_start_end, end_pos):
+        """
+        Inputs:
+        - h_states: Tensor of shape (num_layers, batch, hidden_dim)
+        - seq_start_end: A list of tuples which delimit sequences within batch
+        - end_pos: Tensor of shape (batch, 2)
+        Output:
+        - pool_h: Tensor of shape (batch, bottleneck_dim)
+        """
+        pool_h = []
+        for _, (start, end) in enumerate(seq_start_end):
+            start = start.item()
+            end = end.item()
+            num_ped = end - start
+            curr_hidden = h_states.view(-1, self.hidden_dim)[start:end]
+            curr_end_pos = end_pos[start:end]
+            # Repeat -> H1, H2, H1, H2
+            curr_hidden_1 = curr_hidden.repeat(num_ped, 1)
+            # Repeat position -> P1, P2, P1, P2
+            curr_end_pos_1 = curr_end_pos.repeat(num_ped, 1)
+            # Repeat position -> P1, P1, P2, P2
+            curr_end_pos_2 = self.repeat(curr_end_pos, num_ped)
+            curr_rel_pos = curr_end_pos_1 - curr_end_pos_2
+            curr_rel_embedding = self.spatial_embedding(curr_rel_pos)
+            mlp_h_input = torch.cat([curr_rel_embedding, curr_hidden_1], dim=1)
+            curr_pool_h = self.mlp_pre_pool(mlp_h_input)
+            curr_pool_h = curr_pool_h.view(num_ped, num_ped, -1).max(1)[0]
+            pool_h.append(curr_pool_h)
+        pool_h = torch.cat(pool_h, dim=0)
+        return pool_h
