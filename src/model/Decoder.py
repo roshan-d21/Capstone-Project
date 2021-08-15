716523d3dcf3325331b01a7efe7c7948747ea8d6
Roshan Daivajna <roshan.daivajna@gmail.com>
2021-08-15 10:59:35 +0530

    Add Decoder code

--- src/model/Decoder.py
+++ src/model/Decoder.py
@@ -1,11 +1,82 @@
 import torch
 from torch import nn
 
-device = 'cuda' if torch.cuda.is_available else 'cpu'
-
 class Decoder(nn.Module):
-    def __init__(self):
+    def __init__(
+        self, seq_len, embedding_dim=64, hidden_dim=128, mlp_dim=1024, num_layers=1,
+        pool_every_timestep=True, dropout=0.0, bottleneck_dim=1024,
+        activation='relu', batch_norm=True, pooling_type='pool_net',
+        neighborhood_size=2.0, grid_size=8
+    ):
         super(Decoder, self).__init__()
-    
-    def forward(self):
-        pass
+
+        self.seq_len = seq_len
+        self.mlp_dim = mlp_dim
+        self.hidden_dim = hidden_dim
+        self.embedding_dim = embedding_dim
+        self.pool_every_timestep = pool_every_timestep
+
+        self.decoder = nn.LSTM(
+            embedding_dim, hidden_dim, num_layers, dropout=dropout
+        )
+
+        if pool_every_timestep:
+            self.pool_net = PoolHiddenNet(
+                embedding_dim=self.embedding_dim,
+                hidden_dim=self.hidden_dim,
+                mlp_dim=mlp_dim,
+                bottleneck_dim=bottleneck_dim,
+                activation=activation,
+                batch_norm=batch_norm,
+                dropout=dropout
+            )
+
+            mlp_dims = [hidden_dim + bottleneck_dim, mlp_dim, hidden_dim]
+            self.mlp = make_mlp(
+                mlp_dims,
+                activation=activation,
+                batch_norm=batch_norm,
+                dropout=dropout
+            )
+
+        self.spatial_embedding = nn.Linear(2, embedding_dim)
+        self.hidden2pos = nn.Linear(hidden_dim, 2)
+
+    def forward(self, last_pos, last_pos_rel, state_tuple, seq_start_end):
+        """
+        Inputs:
+        - last_pos: Tensor of shape (batch, 2)
+        - last_pos_rel: Tensor of shape (batch, 2)
+        - state_tuple: (hh, ch) each tensor of shape (num_layers, batch, hidden_dim)
+        - seq_start_end: A list of tuples which delimit sequences within batch
+        Output:
+        - pred_traj: tensor of shape (self.seq_len, batch, 2)
+        """
+        batch = last_pos.size(0)
+        pred_traj_fake_rel = []
+        decoder_input = self.spatial_embedding(last_pos_rel)
+        decoder_input = decoder_input.view(1, batch, self.embedding_dim)
+
+        for _ in range(self.seq_len):
+            output, state_tuple = self.decoder(decoder_input, state_tuple)
+            rel_pos = self.hidden2pos(output.view(-1, self.hidden_dim))
+            curr_pos = rel_pos + last_pos
+
+            if self.pool_every_timestep:
+                decoder_h = state_tuple[0]
+                pool_h = self.pool_net(decoder_h, seq_start_end, curr_pos)
+                decoder_h = torch.cat(
+                    [decoder_h.view(-1, self.hidden_dim), pool_h], dim=1)
+                decoder_h = self.mlp(decoder_h)
+                decoder_h = torch.unsqueeze(decoder_h, 0)
+                state_tuple = (decoder_h, state_tuple[1])
+
+            embedding_input = rel_pos
+
+            decoder_input = self.spatial_embedding(embedding_input)
+            decoder_input = decoder_input.view(1, batch, self.embedding_dim)
+            pred_traj_fake_rel.append(rel_pos.view(batch, -1))
+            last_pos = curr_pos
+
+        pred_traj_fake_rel = torch.stack(pred_traj_fake_rel, dim=0)
+        return pred_traj_fake_rel, state_tuple[0]
